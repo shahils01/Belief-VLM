4270a79ba114622a86147233cab8da2ec4501630
shahils01 <shahil.shaik7@gmail.com>
2026-03-25 10:54:34 -0400

    no mjaor changes

--- train_ppo_vqa.sh
+++ train_ppo_vqa.sh
@@ -1,29 +1,32 @@
-VL_MODEL_PRESET="${VL_MODEL_PRESET:-internvl3_5_1b}"
+VL_MODEL_PRESET="${VL_MODEL_PRESET:-internvl3_5_2b}"
 VIDEO_ROOT="${VIDEO_ROOT:-/scratch/shahils/hd_epic_dataset/videos/HD-EPIC/Videos}"
 ANNOTATION_PATH="${ANNOTATION_PATH:-/scratch/shahils/hd_epic_dataset/hd-epic-annotations/vqa-benchmark/fine_grained_why_recognition.json}"
 METADATA_ROOT="${METADATA_ROOT:-/scratch/shahils/hd_epic_dataset/HD-EPIC Intermediate Data}"
-VLM_CHECKPOINT="${VLM_CHECKPOINT:-}"
+VLM_CHECKPOINT="${VLM_CHECKPOINT:-/scratch/shahils/Belief-VLM/checkpoints_belief_hd_epic_ddp_07/ckpt_epoch_99.pt}"
 
 CMD=(
-  accelerate launch --num_processes 1 train_ppo_vqa.py
+  accelerate launch --num_processes 4 train_ppo_vqa.py
   --dataset_type hd_epic_local
   --video_root "$VIDEO_ROOT"
   --metadata_root "$METADATA_ROOT"
   --annotation_path "$ANNOTATION_PATH"
   --video_extension mp4
-  --val_ratio 0.01
+  --val_ratio 0.1
   --batch_size 4
-  --num_workers 0
-  --video_frames 20
+  --num_workers 1
+  --video_frames 8
   --mixed_precision bf16
   --allow_tf32
-  --epochs 10
-  --ppo_epochs 4
+  --epochs 500
+  --ppo_epochs 10
   --policy_lr 1e-4
   --vlm_lr 2e-5
+  --train_vlm_with_rl
+  --load_model_only
   --vl_model_preset "$VL_MODEL_PRESET"
   --gradient_checkpointing
-  --save_dir checkpoints_ppo_vqa
+  --save_dir checkpoints_ppo_vqa_pft
+  --resume_checkpoint checkpoints_ppo_vqa_01/ckpt_epoch_22.pt
 )
 
 if [[ -n "$VLM_CHECKPOINT" ]]; then
